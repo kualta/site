7046e92efb6169937eb91c75091e7cbac502ab07
Mathew <contact@kualta.dev>
2023-03-09 20:16:53 +0300

    bump nextjs version

--- site/app/api/articles/route.ts
+++ site/app/api/articles/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import data from 'public/data.json';
 
-
 export async function GET(request: Request) {
     return NextResponse.json(data.articles)
 }
--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -3,39 +3,47 @@ import Link from 'next/link';
 import { roboto_mono } from '../components/fonts';
 
 export const metadata = {
-  title: 'kualta',
-  description: 'kualta\'s official website',
-  icons: {
-    icon: "/icon.png"
-  }
-}
+    title: 'kualta',
+    description: "kualta's official website",
+    icons: {
+        icon: '/icon.png',
+    },
+};
 
-export default function RootLayout(
-  { children }: { children: React.ReactNode }
-) {
-  let texts = [
-    'simply makes things',
-    'makes simple things',
-    'makes things simple',
-  ];
-  let action = texts[Math.floor(Math.random() * texts.length)];
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    let texts = [
+        'simply makes things',
+        'makes simple things',
+        'makes things simple',
+    ];
+    let action = texts[Math.floor(Math.random() * texts.length)];
 
-  let header = (
-    <header className={`flex items-center justify-between border-b py-4 dark:border-neutral-800 ${roboto_mono.className}`}>
-      <Link href={'/'}>
-        <b>kualta</b> {action}
-      </Link>
-      <a href="https://blog.kualta.dev/" target={'_blank'} rel={'noreferrer'}>{`blog >`}</a>
-    </header>
-  );
+    let header = (
+        <header
+            className={`flex items-center justify-between border-b py-4 dark:border-neutral-800 ${roboto_mono.className}`}
+        >
+            <Link href={'/'}>
+                <b>kualta</b> {action}
+            </Link>
+            <a
+                href="https://blog.kualta.dev/"
+                target={'_blank'}
+                rel={'noreferrer'}
+            >{`blog >`}</a>
+        </header>
+    );
 
-  return (
-    <html className={`dark dark:bg-[#111111]`}>
-      <head />
-      <body className='mx-auto flex flex-col min-h-screen max-w-2xl dark:text-stone-200 px-4'>
-        {header}
-        {children}
-      </body>
-    </html>
-  )
+    return (
+        <html className={`dark dark:bg-[#111111]`}>
+            <head />
+            <body className="mx-auto flex flex-col min-h-screen max-w-2xl dark:text-stone-200 px-4">
+                {header}
+                {children}
+            </body>
+        </html>
+    );
 }
