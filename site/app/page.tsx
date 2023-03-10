05add1d62850a98fc8c06b2dc32a59c271984ed9
Mathew <contact@kualta.dev>
2023-03-11 01:09:53 +0300

    import outer components

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,7 +1,15 @@
+import ContactIcons from '@/components/ContactIcons';
+import { Article } from './api/articles/route';
 import { Contact } from './api/contacts/route';
-import ContactIcons from '../../components/ContactIcons'
+import { Project } from './api/projects/route';
 
 const HomePage = async () => {
+    const projects = await getData<Project[]>(
+        'https://kualta.dev/api/projects'
+    );
+    const articles = await getData<Article[]>(
+        'https://kualta.dev/api/articles'
+    );
     const contacts = await getData<Contact[]>(
         'https://kualta.dev/api/contacts'
     );
@@ -20,7 +28,7 @@ async function getData<T>(url: string): Promise<T> {
         throw new Error(response.statusText);
     }
 
-    return await (response.json() as Promise<T>);
+    return await response.json() as Promise<T>;
 }
 
 export default HomePage;
