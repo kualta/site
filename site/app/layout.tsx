ab6606993a86521a8226631615a3ddfce64a752b
Mathew <contact@kualta.dev>
2023-03-11 22:46:47 +0300

    move global css

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,6 +1,6 @@
 import { roboto_mono } from '@/components/fonts';
 import Link from 'next/link';
-import 'styles/globals.css';
+import '@/styles/globals.css';
 
 export const metadata = {
     title: 'kualta',
