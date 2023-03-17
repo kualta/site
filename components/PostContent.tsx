59c0833a4e034eebde3bd3922827b3ae31530e64
Mathew <contact@kualta.dev>
2023-03-17 21:07:18 +0300

    fix paths

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs';
 import matter from 'gray-matter';
 
 const getPostContent = (slug: string) => {
-    const path = process.env.POSTS_PATH || 'posts/';
+    const path = 'posts/';
     const file = `${path}${slug}.md`;
     const content = readFileSync(file, 'utf-8');
     const post = matter(content);
