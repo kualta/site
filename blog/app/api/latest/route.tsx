27acc4e9717f09432174e0f57020fbcbb90078a4
Mathew <contact@kualta.dev>
2023-03-19 18:44:26 +0300

    update latest api

--- blog/app/api/latest/route.tsx
+++ blog/app/api/latest/route.tsx
@@ -2,6 +2,11 @@ import getPostMetadata from '@/../components/PostMetadata';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    let posts = getPostMetadata();
-    return NextResponse.json(posts[0]);
+    let latestPost = getPostMetadata()[0];
+    let linkablePost = {
+        name: latestPost.title,
+        description: latestPost.description,
+        link: 'https://blog.kualta.dev/posts/' + latestPost.filename
+    } 
+    return NextResponse.json(linkablePost);
 }
