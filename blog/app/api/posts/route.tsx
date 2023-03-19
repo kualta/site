39ec2fd998563396d9cbfedf3f6088636101ecfe
Mathew <contact@kualta.dev>
2023-03-19 18:51:37 +0300

    add posts api

---
+++ blog/app/api/posts/route.tsx
@@ -0,0 +1,12 @@
+import getPostMetadata from '@/components/PostMetadata';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+    let posts = getPostMetadata();
+    let linkablePosts = posts.map((post) => ({
+        name: post.title,
+        description: post.description,
+        link: 'https://blog.kualta.dev/posts/' + post.filename,
+    }));
+    return NextResponse.json(linkablePosts);
+}
