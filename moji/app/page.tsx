4d1733e0f5a09d7b20efce7d9025e91922564bbd
Mathew <contact@kualta.dev>
2023-04-08 02:31:54 +0300

    remove button

--- moji/app/page.tsx
+++ moji/app/page.tsx
@@ -4,7 +4,7 @@ import { kaomoji } from '@/public/kaomoji';
 import { useState } from 'react';
 
 const HomePage = () => {
-    let [emote, setEmote] = useState("(´･ω･`)");
+    let [emote, setEmote] = useState('(´･ω･`)');
 
     let chooseEmote = () => {
         setEmote(kaomoji[Math.floor(Math.random() * kaomoji.length)]);
@@ -12,10 +12,9 @@ const HomePage = () => {
 
     return (
         <div className="flex flex-col h-full w-full place-items-center justify-center gap-10 flex-wrap p-4 text-6xl">
-            <div className="m-28">{emote}</div>
-            <button className="text-xl rounded-xl border p-4" onClick={chooseEmote}>
-                another one
-            </button>
+            <div className="m-28 select-text" onClick={chooseEmote}>
+                {emote}
+            </div>
         </div>
     );
 };
