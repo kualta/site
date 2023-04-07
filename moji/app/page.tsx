6621ea431a55d8366e7e7ce70aaf3aac1e70e17b
Mathew <contact@kualta.dev>
2023-04-08 01:20:54 +0300

    remove no ssr add client side rendering

--- moji/app/page.tsx
+++ moji/app/page.tsx
@@ -1,18 +1,17 @@
+'use client';
+
 import { kaomoji } from '@/public/kaomoji';
-import Nossr from './nossr';
 
 const HomePage = () => {
     let chosenOne = kaomoji[Math.floor(Math.random() * kaomoji.length)];
 
     return (
-        <Nossr>
             <div className="flex flex-col h-full w-full place-items-center justify-center gap-10 flex-wrap p-4 text-6xl">
                 <div className="m-28">{chosenOne}</div>
                 <a className="text-xl rounded-xl border p-4" href="/">
                     another one
                 </a>
             </div>
-        </Nossr>
     );
 };
 
