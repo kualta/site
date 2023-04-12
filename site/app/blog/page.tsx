d5ee06edbc0f9c808889c811af56d2cc19be6b8f
Mathew <contact@kualta.dev>
2023-04-12 22:45:58 +0300

    update middleware

--- site/app/blog/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function BlogPage() {
-    redirect('https://blog.kualta.dev')
-}
--- site/middleware.ts
+++ site/middleware.ts
@@ -6,14 +6,19 @@ export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
     const url = request.nextUrl
 
+    // xyz.kualta.dev 
     if (hostname.startsWith('vids')) {
-        url.hostname = 'kualta.dev'
-        url.pathname = '/vids' + url.pathname
-        NextResponse.redirect(url)
+        return NextResponse.redirect('https://kualta.dev/vids')
     }
+    if (hostname.startsWith('arts')) {
+        return NextResponse.redirect('https://kualta.dev/arts')
+    }
+
+    // ---------------------------------------------------------------
+    // kualta.dev/xyz
     if (pathname.startsWith('/blog')) {
         url.hostname = 'blog.kualta.dev'
-        NextResponse.redirect(url)
+        return NextResponse.redirect(url)
     }
     if (pathname.startsWith("/_next")) return NextResponse.next();
 }
