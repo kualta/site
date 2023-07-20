c7f4fef20834ed0ea76ff415f9215548a129c686
Mathew <contact@kualta.dev>
2023-07-20 14:14:59 +0300

    update api list

--- app/api/page.tsx
+++ app/api/page.tsx
@@ -2,14 +2,14 @@ import fs from "fs";
 import Link from "next/link";
 import path from "path";
 
-function PagesList({ pages }:any) {
+function ApiList({ pages: apis }: any) {
   return (
     <div className="">
-      <ul className="gap-1 flex-col flex ml-4">
-        {pages.map((page: any) => (
-          <li className="hover:underline">
-            <Link href={page} key={page}>
-              {page}
+      <ul className="gap-2 text-sm sm:text-base flex-col flex ml-4">
+        {apis.map((api: any) => (
+          <li className="hover:underline font-mono">
+            <Link href={api} key={api}>
+              {api}
             </Link>
           </li>
         ))}
@@ -20,12 +20,16 @@ function PagesList({ pages }:any) {
 
 export default function page() {
   const apiDirectory = path.join(process.cwd(), "app/api");
-  const apis = fs.readdirSync(apiDirectory).map((file) => `/api/${file.replace(/\.js$/, "")}`);
+
+  const apis = fs
+    .readdirSync(apiDirectory)
+    .filter((path) => !path.endsWith(".tsx"))
+    .map((path) => `/api/${path}`)
 
   return (
     <>
-      <h1>Available APIs</h1>
-      <PagesList pages={apis} />
+      {/* <h1 className="text-bold text-md sm:text-lg">Available APIs:</h1> */}
+      <ApiList pages={apis} />
     </>
   );
 }
