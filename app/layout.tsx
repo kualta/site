e3bf079a5ac49a9cdc26e2ecb5ac5f68c042972d
kualta <contact@kualta.dev>
2023-10-24 17:30:35 +0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle, { BackButton } from "@/components/MetaButton";
 import "styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "kualta",
@@ -49,6 +50,7 @@ export default function RootLayout({
           <div className="absolute top-0 right-0  border-t-2 border-r-2 m-4 p-4 rounded-tr-lg" />
           <div className="absolute top-0 left-0  border-t-2 border-l-2 m-4 p-4  rounded-tl-lg" />
         </div>
+        <Analytics />
       </body>
     </html>
   );
