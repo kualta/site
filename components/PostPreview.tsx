950d0de0e064b877084cbc36e94a046222e3899b
kualta <contact@kualta.dev>
2023-10-31 12:13:06 +0300

    fix post links

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -7,22 +7,16 @@ const PostPreview = (props: PostMetadata) => {
   return (
     <div className="w-fit sm:w-full">
       <Card key={props.description}>
-        <Link
-          href={`/posts/${props.filename}`}
-          className="flex flex-col sm:flex-row items-center gap-6 sm:gap-4 w-fit"
-        >
-          <div className="grow h-36 sm:h-28 aspect-video relative">
-            <Image
-              src={props.preview}
-              alt={props.title}
-              fill={true}
-              className="rounded-xl object-contain"
-            />
-          </div>
-          <div className="flex flex-col gap-1 sm:gap-2">
-            <h1 className="font-extrabold text-xl">{props.title}</h1>
-            <p className="text-base flex-grow">{props.description}</p>
-            <p className="text-sm text-secondary-text">{props.date}</p>
+        <Link href={`/posts/${props.filename}`} className="w-full">
+          <div className="flex flex-col sm:flex-row items-center gap-6 sm:gap-4 w-fit">
+            <div className="grow h-36 sm:h-28 aspect-video relative">
+              <Image src={props.preview} alt={props.title} fill={true} className="rounded-xl object-contain" />
+            </div>
+            <div className="flex flex-col gap-1 sm:gap-2 w-full">
+              <h1 className="font-extrabold text-xl">{props.title}</h1>
+              <p className="text-base flex-grow">{props.description}</p>
+              <p className="text-sm text-secondary-text">{props.date}</p>
+            </div>
           </div>
         </Link>
       </Card>
