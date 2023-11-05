734c23648e0304dbe5d498475473c8ed3b5a45f3
kualta <contact@kualta.dev>
2023-11-05 12:47:16 +0300

    don't force dynamic

--- app/pics/page.tsx
+++ app/pics/page.tsx
@@ -1,8 +1,8 @@
 import { getRandomImage } from "prisma/dataFetch";
-import { ImagePage } from "./ImagePage";
+import Image from "next/image";
 
 export const revalidate = 0;
-const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 async function page() {
   const { url } = await (await getRandomImage()).json();
@@ -10,7 +10,16 @@ async function page() {
   return (
     <div className="flex flex-col justify-center place-items-center gap-8 text-3xl">
       <h1>THIS KUOLLUT DOES NOT EXIST</h1>
-      <ImagePage url={url} />
+      <div className="w-64 h-auto">
+        <Image
+          src={url}
+          className="rounded-lg"
+          style={{ width: "100%", height: "auto" }}
+          width={1000}
+          height={1000}
+          alt="a random kuollut"
+        />
+      </div>
     </div>
   );
 }
