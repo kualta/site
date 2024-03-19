37be17e4ce0a6b1e2f07ef833b4daa53b58b32c3
kualta <contact@kualta.dev>
2024-03-19 15:05:14 +0300

    update join page

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -19,12 +19,12 @@ export default function page() {
         <CornersScope top_left={false} bottom_right={false} />
         <h1 className="text-3xl font-bold text-center">Subscribe</h1>
         <EmailSubscription />
-        <p className="text-xl text-center p-4 pt-0">
+        <p className="text-lg sm:text-xl text-center p-4 pt-0">
           Kurier will deliver latest news <br /> and events right into your mailbox.
         </p>
         <Sticker
           name="kurier"
-          className="absolute w-32 h-32 md:w-48 md:h-48 -right-10 -bottom-10 md:-right-24 md:-bottom-24"
+          className="hidden sm:flex absolute w-32 h-32 md:w-48 md:h-48 -right-10 -bottom-10 md:-right-24 md:-bottom-24"
         />
       </div>
 
@@ -32,7 +32,7 @@ export default function page() {
         <CornersScope top_right={false} bottom_left={false} />
         <h1 className="text-3xl font-bold text-center">Notifications</h1>
         <div className="px-4 min-w-0">
-          <div className="flex flex-col sm:flex-row gap-20 py-4 sm:px-4 place-content-around items-center ">
+          <div className="flex flex-wrap gap-10 sm:gap-20 py-4 sm:px-4 place-content-around items-center ">
             <a
               href="https://twitter.com/kualts"
               className="flex flex-col justify-center items-center hover:drop-shadow-glow hover:transition-all hover:duration-300 hover:cursor-pointer"
@@ -68,7 +68,7 @@ export default function page() {
             </div> */}
           </div>
         </div>
-        <div className="text-xl place-items-center justify-center flex flex-row gap-2">
+        <div className="text-lg sm:text-xl place-items-center justify-center flex flex-row gap-2">
           Follow to not <b className="drop-shadow-glow-sm">miss a thing</b>
         </div>
       </div>
