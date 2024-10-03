708274049257b3c3455d9675e4af1779e28d66aa
kualta <contact@kualta.dev>
2024-10-03 11:54:00 +0400

    deprecate vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import "styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+
 import { fredoka } from "styles/fonts";
 import { IsChristmas, Snow } from "@/components/Effects";
 import { CornersScope } from "@/components/CornersScope";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import Head from "next/head";
 import { Header } from "@/components/Header";
 import { AboutButton, NotificationsButton } from "@/components/MetaButton";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://kualta.dev"),
@@ -71,10 +72,11 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth dark">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Script defer src="https://stats.pingpad.io/script.js" data-website-id="09665935-c691-42bc-b2ba-d02329c44f3e" />
       </Head>
       <body
-        className={`bg-bg dark:bg-dark-bg disable-scrollbars text-text transition-all 
-        ease-in-out delay-150 dark:text-dark-text place-items-center 
+        className={`bg-bg dark:bg-dark-bg disable-scrollbars text-text transition-all
+        ease-in-out delay-150 dark:text-dark-text place-items-center
         min-h-screen flex flex-col justify center relative ${fredoka.className}`}
       >
         <div className="noise" />
@@ -94,11 +96,10 @@ export default function RootLayout({
           <CornersScope />
         </div>
 
-        <Analytics />
         <Toaster
           toastOptions={{
-            className: `p-3 min-w-0 rounded-lg bg-secondary dark:bg-dark-secondary drop-shadow-md  
-            transition duration-100 border-2 border-secondary dark:border-dark-secondary 
+            className: `p-3 min-w-0 rounded-lg bg-secondary dark:bg-dark-secondary drop-shadow-md
+            transition duration-100 border-2 border-secondary dark:border-dark-secondary
             hover:dark:border-dark-primary hover:border-primary text-text dark:text-dark-text`,
           }}
         />
